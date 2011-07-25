b812a99aca939d825359550c9fa924b2b8376116
Kevin Griffin <griffin.kev@gmail.com>
2011-07-25 12:09:36 -0400

    consuming the intent after I have processed it Setting the 'current intent' when a new one is received through 'onNewIntent' Comments showing the pending intent is configured correctly

--- src/com/moominland/readie/ListTags.java
+++ src/com/moominland/readie/ListTags.java
@@ -8,15 +8,6 @@ import android.content.IntentFilter;
 import android.content.IntentFilter.MalformedMimeTypeException;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
-import android.nfc.tech.IsoDep;
-import android.nfc.tech.MifareClassic;
-import android.nfc.tech.MifareUltralight;
-import android.nfc.tech.Ndef;
-import android.nfc.tech.NdefFormatable;
-import android.nfc.tech.NfcA;
-import android.nfc.tech.NfcB;
-import android.nfc.tech.NfcF;
-import android.nfc.tech.NfcV;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.util.Log;
@@ -26,11 +17,11 @@ public class ListTags extends Activity {
 
 	private NfcAdapter nfcAdapter;
 	private PendingIntent pendingIntent;
-	private String[][] techLists;
+	private String[][] techLists = new String[][] {};
 
 	private TextView textview;
 
-	private static final String TAG = "com.moominland.activities.ListTags";
+	private static final String TAG = "Readie";
 	private IntentFilter[] intentFilters;
 
 	@Override
@@ -41,10 +32,9 @@ public class ListTags extends Activity {
 		textview.setText("Scan a tag");
 		setContentView(textview);
 
-		nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+		nfcAdapter = NfcAdapter.getDefaultAdapter(ListTags.this);
 		pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,
-				getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
-//				getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+				getClass()).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
 
 		IntentFilter ndefFilter = new IntentFilter(
 				NfcAdapter.ACTION_NDEF_DISCOVERED);
@@ -53,58 +43,51 @@ public class ListTags extends Activity {
 		} catch (MalformedMimeTypeException e) {
 			Log.e(TAG,"MalformedMimeTypeException setting mime type");
 		}
-		
-		IntentFilter techFilter = new IntentFilter(
-				NfcAdapter.ACTION_TECH_DISCOVERED);
-		intentFilters = new IntentFilter[] { ndefFilter, techFilter };
 
-		techLists = new String[][] { { MifareClassic.class.getName() },
-				{ MifareUltralight.class.getName() },
-				{ IsoDep.class.getName() }, { NfcA.class.getName() },
-				{ NfcB.class.getName() }, { NfcF.class.getName() },
-				{ NfcV.class.getName() }, { Ndef.class.getName() },
-				{ NdefFormatable.class.getName() } };
-	}
+		intentFilters = new IntentFilter[] { ndefFilter };
+    }
 
 	@Override
 	public void onResume() {
 		super.onResume();
-		Log.i(TAG, "**** RESUME");
+		Log.i(TAG, "resumed " + pendingIntent + " " + intentFilters + " " + techLists);
+        for (IntentFilter intentFilter : intentFilters) {
+            Log.d(TAG, "actions " + intentFilter.getAction(0));
+            Log.d(TAG, "data type " + intentFilter.getDataType(0));
+        }
 		nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilters,
 				techLists);
-		Log.i(TAG, getIntent().getAction());
+    }
 
-		String action = getIntent().getAction();
-//		if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
-//			Log.i(TAG, "Action detected");
-//			updateTagDetailsFromIntent(getIntent());
-//		}
-		
-		if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
-			Log.i(TAG, "Action detected");
-			updateTagDetailsFromIntent(getIntent());
-//			setIntent(new Intent());
-		}
-	}
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "I see the correct NDEF intent here, in the log");
+    }
 
 	@Override
 	public void onNewIntent(Intent intent) {
-		Log.i(TAG, "NEW INTENT");
-		updateTagDetailsFromIntent(intent);
+		Log.i(TAG, "new intent");
+        /**
+         * http://developer.android.com/reference/android/app/Activity.html#onNewIntent(android.content.Intent)
+         * Note that getIntent() still returns the original Intent.
+         * You can use setIntent(Intent) to update it to this new Intent.
+         */
+        setIntent(intent);
+		updateTagDetailsFromIntent();
 	}
 
 	@Override
 	public void onPause() {
 		super.onPause();
-		Log.d(TAG, "pasuing " + getIntent().getAction());
 		nfcAdapter.disableForegroundDispatch(this);
 	}
 
-	private void updateTagDetailsFromIntent(Intent intent) {
+	private void updateTagDetailsFromIntent() {
 		Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 		v.vibrate(100);
 		
-		Tag detectedTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+		Tag detectedTag = getIntent().getParcelableExtra(NfcAdapter.EXTRA_TAG);
 		
 		textview.setText("");
 		textview.setText("Discovered tag supporting the following tech:\n");
@@ -115,6 +98,8 @@ public class ListTags extends Activity {
 			techs += (s + "\n");
 			Log.i(TAG, s);
 		}
-		textview.append(techs);		
+		textview.append(techs);
+        //we've processed this intent, lets remove it.
+        setIntent(new Intent());
 	}
 }
