6b136e68058949a7051466bf045c9489c9ca3bad
Kevin Griffin <griffin.kev@gmail.com>
2011-08-10 09:37:35 -0400

    still trying to get a complete demo :)

--- src/com/moominland/readie/ListTags.java
+++ src/com/moominland/readie/ListTags.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.IntentFilter.MalformedMimeTypeException;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
@@ -14,18 +13,14 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.text.MessageFormat;
-
 public class ListTags extends Activity {
 
     private NfcAdapter nfcAdapter;
-    private PendingIntent pendingIntent;
     private String[][] techLists = new String[][]{};
 
     private TextView textview;
 
     private static final String TAG = "Readie";
-    private IntentFilter[] intentFilters;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,33 +29,12 @@ public class ListTags extends Activity {
         textview = new TextView(this);
         textview.setText("Scan a tag");
         setContentView(textview);
-
-        nfcAdapter = NfcAdapter.getDefaultAdapter(ListTags.this);
-        pendingIntent = createPendingResult(1000, new Intent(this, getClass()), PendingIntent.FLAG_UPDATE_CURRENT);
-//		pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,
-//				getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
-
-        IntentFilter ndefFilter = new IntentFilter(
-                NfcAdapter.ACTION_NDEF_DISCOVERED);
-        try {
-            ndefFilter.addDataType("text/foo");
-        } catch (MalformedMimeTypeException e) {
-            Log.e(TAG, "MalformedMimeTypeException setting mime type");
-        }
-
-        intentFilters = new IntentFilter[]{ndefFilter};
-
-        if (getIntent() != null) {
-            updateTagDetailsFromIntent();
-        }
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         Log.d(TAG, "onActivityResult");
         if (requestCode == 1000) {
-            Log.d(TAG, "winning");
-            Log.d(TAG, data.getAction());
             setIntent(data);
             updateTagDetailsFromIntent();
         }
@@ -69,12 +43,24 @@ public class ListTags extends Activity {
     @Override
     public void onResume() {
         super.onResume();
-        Log.i(TAG, MessageFormat.format("resumed {0} {1} {2}", pendingIntent, intentFilters, techLists));
-        for (IntentFilter intentFilter : intentFilters) {
-            Log.d(TAG, "actions " + intentFilter.getAction(0));
-            Log.d(TAG, "data type " + intentFilter.getDataType(0));
+        Log.d(TAG, "resuming " + getIntent());
+        nfcAdapter = NfcAdapter.getDefaultAdapter(ListTags.this);
+        PendingIntent pendingIntent = createPendingResult(1000, new Intent(this, getClass()), PendingIntent.FLAG_UPDATE_CURRENT);
+
+        IntentFilter ndefFilter = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+
+        try {
+            ndefFilter.addDataType("text/foo");
+        } catch (IntentFilter.MalformedMimeTypeException e) {
+            Log.e(TAG, "MalformedMimeTypeException setting mime type");
         }
-        Log.d(TAG, getIntent().toString());
+
+        IntentFilter[] intentFilters = new IntentFilter[]{ndefFilter};
+
+        if (getIntent() != null) {
+            updateTagDetailsFromIntent();
+        }
+
         nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilters,
                 techLists);
     }
@@ -101,16 +87,16 @@ public class ListTags extends Activity {
         if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
             textview.setText("");
             Parcelable[] rawData = getIntent().getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-            NdefMessage[] msgs = null;
+            NdefMessage[] msgs;
             if (rawData != null) {
                 msgs = new NdefMessage[rawData.length];
                 for (int i = 0; i < rawData.length; i++) {
                     msgs[i] = (NdefMessage) rawData[i];
                 }
             } else {
-                // Unknown tag type
+                // Empty tag type
                 byte[] empty = new byte[]{};
-                NdefRecord record = new NdefRecord(NdefRecord.TNF_UNKNOWN, empty, empty, empty);
+                NdefRecord record = new NdefRecord(NdefRecord.TNF_EMPTY, empty, empty, empty);
                 NdefMessage msg = new NdefMessage(new NdefRecord[]{record});
                 msgs = new NdefMessage[]{msg};
             }
